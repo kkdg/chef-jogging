a4b7c192634defd53e87bccd58c214940aa25455
Kwag De Gi <code@skyaperture.com>
2015-02-24 11:27:53 +0900

    setup index page for new server

--- 0224/web.rb
+++ 0224/web.rb
@@ -3,3 +3,11 @@ package 'apache2'
 service 'apache2' do
 	action [:start, :enable]
 end
+
+file '/var/www/html/index.html' do
+	content '<html>
+	<body>
+		<h1>Hi world</h1>
+	</body>
+    </html>'
+end
